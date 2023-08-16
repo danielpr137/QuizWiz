2836f7e33b3391b8db5c4544279233ff1cc4c670
Ido <ido.ofir@gmail.com>
2023-08-16 11:25:03 +0300

    PDF to txt conversion

--- Raw-LLM.py
+++ Raw-LLM.py
@@ -1,1 +1,14 @@
-print("hi")
+import os.path
+import PyPDF2
+
+
+# assuming the PDF file is located in a dedicated-temporary folder. the .txt file will be saved in the same folder.
+def convertPDFtoTXT(path):
+    with open(path, 'rb') as PDFFile:
+        pdfReader = PyPDF2.PdfFileReader(PDFFile)
+        numOfPages = pdfReader.numPages
+        pdfObj = pdfReader.getPage(numOfPages + 1)
+        text = pdfObj.extractText()
+        textFilePath = os.path.join(os.path.dirname(path), 'text.txt')
+        with open(textFilePath, 'w') as textFile:
+            textFile.writelines(text)
