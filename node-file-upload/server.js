6dfa53b3758fbcf06513d99e354199a739d742dd
Guy-Ben-Yosef <guyben@campus.technion.ac.il>
2023-08-21 21:24:47 +0300

    Google login button (visual button only)

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -3,8 +3,50 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
+// Google login
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
 const app = express();
 const port = 3000;
+
+///////////////////////////////////////////////
+app.use(session({
+    secret: 'your_secret_string_here',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: 'YOUR_GOOGLE_CLIENT_ID',
+    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    // You can store user details (profile) in your database here.
+    return done(null, profile);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    // Successful login.
+    res.redirect('/'); // Or wherever you want to redirect the user after a successful login.
+});
+///////////////////////////////////////////////
 
 // Sotrage settings
 const storage = multer.diskStorage({
