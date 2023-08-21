51d024e409fff236964ac797c019fd98e3f0dc98
Guy-Ben-Yosef <guyben@campus.technion.ac.il>
2023-08-21 17:57:56 +0300

    I think I finished this very poor server

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 
-// Define storage settings for multer
+// Sotrage settings
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
@@ -14,4 +14,18 @@ const storage = multer.diskStorage({
     },
 });
 
-a
+const upload = multer({ storage: storage});
+
+// Get index HTML
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Handle file upload including feedback
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+  });
